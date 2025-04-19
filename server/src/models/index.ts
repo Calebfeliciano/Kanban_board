04f50fd5fb3fa414f59571c8a78f88c6612c5966
{your_name} <{Caleb.feliciano11@gmial.com}>
2025-04-19 03:16:55 -0400

    reverting to original build

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,34 +5,19 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL, {
-      dialect: 'postgres',
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-        decimalNumbers: true,
-      },
-    })
-  : new Sequelize(
-      process.env.DB_NAME || '',
-      process.env.DB_USER || '',
-      process.env.DB_PASSWORD || '',
-      {
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT) || 5432,
-        dialect: 'postgres',
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-          decimalNumbers: true,
-        },
-      }
-    );
+const sequelize = new Sequelize(
+  process.env.DB_NAME || '',
+  process.env.DB_USER || '',
+  process.env.DB_PASSWORD || '',
+  {
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  }
+);
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
